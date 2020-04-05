3861f3ec9da2f10f45327e08e5502c00f0286433
Matheus Carvalho <33005830+theuscarvalho@users.noreply.github.com>
2020-04-04 22:22:27 -0300

    Add files via upload

---
+++ atualiza_coordenadas.php
@@ -0,0 +1,49 @@
+<?php
+$apiKey = 'Digite-Aqui-o-Token-do-Google';
+# Crie um token no google geocodig: https://developers.google.com/maps/documentation/geocoding/get-api-key?hl=pt-br
+///////////////////////////////////////////////////////
+###################NAO MODIFICAR NADA A BAIXO.########################
+# - autor: Matheus Carvalho
+# - Todos direitos reservados.
+# - www.matheuscarvalho.com.br
+$hostname = "localhost";
+$bancodedados = "mkradius";
+$usuario = "root";
+$senha = "vertrigo";
+$mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
+if ($mysqli->connect_errno) {
+    echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+$sql = "select id, endereco_res, numero_res, bairro_res, cidade_res, estado_res, coordenadas from sis_cliente";
+$con_sql = $mysqli->query($sql) or die($mysqli->error);
+while ($dados_sql = $con_sql->fetch_array()) {
+   $id = $dados_sql['id'];
+   $logradouro = $dados_sql['endereco_res'];
+   $numero = $dados_sql['numero_res'];
+   $bairro = $dados_sql['bairro_res'];
+   $cidade = $dados_sql['cidade_res'];
+   $uf = $dados_sql['estado_res'];
+   $ver_coord = $dados_sql['coordenadas'];
+   $resul_coord = strlen($ver_coord);
+   if ($ver_coord >= $resul_coord){
+   $endA = $numero." + ".$logradouro." + ".$bairro." + ".$cidade." + ".$uf;
+   $endB = str_replace(" ", "+", $endA);
+   $address = utf8_encode($endB);
+   $geo = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?key='.$apiKey.'&address='.urlencode($address).'&sensor=false');
+   $geo = json_decode($geo, true);
+   if ($geo['status'] == 'OK') {
+     $latitude = $geo['results'][0]['geometry']['location']['lat'];
+     $longitude = $geo['results'][0]['geometry']['location']['lng'];
+     $result_final = $latitude.", ".$longitude;
+      $sql = "UPDATE sis_cliente SET coordenadas = '".$result_final. "' where Id = '".$id."'";
+      $mysqli->query($sql) or die($mysqli->error);
+   }
+   else {
+      echo "Erro na conexao com o banco de dados.";
+      }
+   echo $id."- ".$result_final;
+} else{
+ echo "Cadastro já atualizado.";
+}
+}
+?>
