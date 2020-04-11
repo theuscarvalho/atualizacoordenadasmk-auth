cc865140c0ca5bcbf602a7485dcda2c4d623b27a
Matheus Carvalho <33005830+theuscarvalho@users.noreply.github.com>
2020-04-10 21:22:03 -0300

    Add files via upload
    Add cep na consulta de dados

--- atualiza_coordenadas.php
+++ atualiza_coordenadas.php
@@ -14,7 +14,7 @@ $mysqli = new mysqli($hostname, $usuario, $senha, $bancodedados);
 if ($mysqli->connect_errno) {
     echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-$sql = "select id, endereco_res, numero_res, bairro_res, cidade_res, estado_res, coordenadas from sis_cliente";
+$sql = "select id, endereco_res, numero_res, bairro_res, cidade_res, estado_res, cep_res, coordenadas from sis_cliente";
 $con_sql = $mysqli->query($sql) or die($mysqli->error);
 while ($dados_sql = $con_sql->fetch_array()) {
    $id = $dados_sql['id'];
@@ -23,10 +23,11 @@ while ($dados_sql = $con_sql->fetch_array()) {
    $bairro = $dados_sql['bairro_res'];
    $cidade = $dados_sql['cidade_res'];
    $uf = $dados_sql['estado_res'];
+   $cep = $dados_sql['cep_res'];
    $ver_coord = $dados_sql['coordenadas'];
    $resul_coord = strlen($ver_coord);
    if ($resul_coord <= '15'){
-   $endA = $numero." + ".$logradouro." + ".$bairro." + ".$cidade." + ".$uf;
+   $endA = $numero." + ".$logradouro." + ".$bairro." + ".$cidade." + ".$uf." + ".$cep;
    $endB = str_replace(" ", "+", $endA);
    $address = utf8_encode($endB);
    $geo = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?key='.$apiKey.'&address='.urlencode($address).'&sensor=false');
